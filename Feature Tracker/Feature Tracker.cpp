8a07d116f2fff7fd413bbcc42971bc60696f71ec
Tharindu Bandara <Tharindu Bandara@THARINDU-BANDAR>
2017-04-02 02:13:00 +0530

    Tracker branch first commit

--- Feature Tracker/Feature Tracker.cpp
+++ Feature Tracker/Feature Tracker.cpp
@@ -27,22 +27,7 @@ int main()
 
 	cv::Mat Corners = Tracker.HarrisCorner(image, 0.03, 0.08);
 
-	for (int x = 0;  x < Corners.cols; x++) {
-		for (int y = 0; y < Corners.rows; y++) {
-			//std::cout << x << "," << y << std::endl;
-			if ((int)Corners.at<uchar>(y, x) != 0)
-			{
-				cv::circle(image, cv::Point(x,y), 7, cv::Scalar(0, 0, 255), 2);
-			}
-		}
-	}
 
-	std::cout << "Processing Done..." << std::endl;
-
-	cv::imshow("image", image);
-	//cv::imshow("image2", Corners);
-
-	cv::waitKey(0); // sfdfdd
     return 0;
 }
 
